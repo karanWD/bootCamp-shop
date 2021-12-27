781e39fb8426158098b9c9c61a39de4eb2b14476
karan_shams_pirzadeh <karanshamspirzadeh@gmail.com>
2021-12-27 17:45:26 +0330

    implement store and root-reducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Home from "./pages/Home"
 import Layout from "./components/Layout/Layout";
 import "swiper/swiper-bundle.css"
 import "./app.css"
-
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
 
 
 function App() {
     return (
         <div className="App">
-            <Layout>
-                <Home/>
-            </Layout>
+            <Provider store={store}>
+                <Layout>
+                    <Home/>
+                </Layout>
+            </Provider>
         </div>
     );
 }
---
+++ src/components/Count/Count.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Count = () => {
+    return (
+        <>
+            <button>+</button>
+            <div></div>
+            <button>-</button>
+        </>
+    )
+}
+
+export default Count
